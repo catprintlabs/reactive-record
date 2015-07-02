e2b87c3d33f96244656fb82d0c4c8e75110d0bc3
Mitch VanDuyn <mitch@catprint.com>
2015-07-02 11:47:38 -0400

    have to include cache on server

--- lib/reactive_record.rb
+++ lib/reactive_record.rb
@@ -10,9 +10,10 @@ else
   require "opal"
   require "reactive_record/version"
   require "reactive_record/engine"
+  require "reactive_record/cache"
   begin  # can't figure out how to make this work when gem is released...
-    require "../../config/routes.rb"
-    require "../../app/controllers/reactive_record/reactive_record_controller"
+    #require "../../config/routes.rb"
+    #require "../../app/controllers/reactive_record/reactive_record_controller"
   rescue
   end
 
