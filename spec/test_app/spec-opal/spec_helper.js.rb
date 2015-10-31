2cf942a2f45147c58a8273eb5a45d85e76896f66
Mitch VanDuyn <mitch@catprint.com>
2015-10-31 10:31:55 -0400

    initialized cookies in test

--- spec/test_app/spec-opal/spec_helper.js.rb
+++ spec/test_app/spec-opal/spec_helper.js.rb
@@ -12,11 +12,12 @@ require 'models'
 
 #Opal::RSpec::Runner.autorun
 Document.ready? do
+  `$.cookie('acting_user', null, { path: '/' })`
   Opal::RSpec::Runner.autorun
 end
 
 class Object
-    
+
   def use_case(*args, &block)
     describe(*args) do
       clear_opal_rspec_runners
@@ -45,36 +46,36 @@ class Object
       last_promise.resolve
     end
   end
-      
+
 end
 
 module Opal
   module RSpec
     module AsyncHelpers
       module ClassMethods
-        
+
         def self.set_current_promise(instance, promise)
           @current_promise = promise
           @current_promise_test_instance = instance
         end
-        
+
         def self.resolve_current_promise
           @current_promise.resolve if @current_promise
         rescue Exception => e
           raise "test structure error:  Usually this is caused by a use_case test that has only a first_it an no other tests.  Check the use_case that ran just before this one."
         end
-        
+
         def self.get_current_promise_test_instance
           @current_promise_test_instance
         end
-        
+
         #alias_method :old_it, :it
-        
+
         #def it(*args, &block)
         #  @previous_promise = new_promise
         #  old_it(*args, &block)
         #end
-        
+
         def opal_rspec_runners
           @opal_rspec_runners
         end
@@ -86,50 +87,50 @@ module Opal
         def opal_rspec_push_runner(type, title, opts, block)
           @opal_rspec_runners << [type, title, opts, block, Promise.new]
         end
-                  
-        
+
+
         def first_it(title, opts = {}, &block)
           opal_rspec_push_runner("first_it", title, opts, block)
         end
-        
+
         def now_it(title, opts = {}, &block)
           opal_rspec_push_runner("now_it", title, opts, block)
         end
-        
+
         def and_it(title, opts = {}, &block)
           opal_rspec_push_runner("and_it", title, opts, block)
         end
-        
+
         def finally(title, opts = {}, &block)
           opal_rspec_push_runner("finally", title, opts, block)
         end
-        
+
         def rendering(title, &block)
           klass = Class.new do
-            
+
             include React::Component
-            
+
             def self.block
               @block
             end
-            
+
             def self.name
               "dummy class"
             end
-            
+
             backtrace :on
-            
+
             def render
               instance_eval &self.class.block
             end
-            
+
             def self.should_generate(opts={}, &block)
               sself = self
               @self.async(@title, opts) do
                 expect_component_to_eventually(sself, &block)
               end
             end
-            
+
             def self.should_immediately_generate(opts={}, &block)
               sself = self
               @self.it(@title, opts) do
@@ -138,7 +139,7 @@ module Opal
                 expect((element and context.instance_exec(element, &block))).to be(true)
               end
             end
-            
+
           end
           klass.instance_variable_set("@block", block)
           klass.instance_variable_set("@self", self)
@@ -151,7 +152,7 @@ module Opal
 end
 
 module ReactTestHelpers
-  
+
   `var ReactTestUtils = React.addons.TestUtils`
 
   def renderToDocument(type, options = {})
@@ -174,7 +175,7 @@ module ReactTestHelpers
   def isElementOfType(element, type)
     `React.addons.TestUtils.isElementOfType(#{element.to_n}, #{type.cached_component_class})`
   end
-  
+
   def build_element(type, options)
     component = React.create_element(type, options)
     element = `ReactTestUtils.renderIntoDocument(#{component.to_n})`
@@ -184,63 +185,63 @@ module ReactTestHelpers
       `$(React.findDOMNode(element))`    # v0.13
     end
   end
-  
-  def expect_component_to_eventually(component_class, opts = {}, &block) 
+
+  def expect_component_to_eventually(component_class, opts = {}, &block)
     # Calls block after each update of a component until it returns true.  When it does set the expectation to true.
     # Uses the after_update callback of the component_class, then instantiates an element of that class
     # The call back is only called on updates, so the call back is manually called right after the
     # element is created.
-    # Because React.rb runs the callback inside the components context, we have to 
+    # Because React.rb runs the callback inside the components context, we have to
     # setup a lambda to get back to correct context before executing run_async.
     # Because run_async can only be run once it is protected by clearing element once the test passes.
     element = nil
-    check_block = lambda do 
+    check_block = lambda do
       context = block.arity > 0 ? self : element
       run_async do
-         element = nil; expect(true).to be(true) 
-      end if element and context.instance_exec(element, &block) 
+         element = nil; expect(true).to be(true)
+      end if element and context.instance_exec(element, &block)
     end
     component_class.after_update { check_block.call  }
     element = build_element component_class, opts
     check_block.call
   end
-  
+
   def test(&block)
-    run_async &block 
+    run_async &block
     Opal::RSpec::AsyncHelpers::ClassMethods.resolve_current_promise
   end
-  
+
   # for the permissions test
-  
+
   def set_acting_user(email)
     `$.cookie('acting_user', #{email}, { path: '/' })`
   end
-    
+
 end
 
 class Promise
-  
+
   def then_test(&block)
-    self.then do |args| 
-      Opal::RSpec::AsyncHelpers::ClassMethods.get_current_promise_test_instance.run_async do 
+    self.then do |args|
+      Opal::RSpec::AsyncHelpers::ClassMethods.get_current_promise_test_instance.run_async do
         yield args
         Opal::RSpec::AsyncHelpers::ClassMethods.resolve_current_promise
       end
     end
   end
-  
-  def while_waiting(&block) 
+
+  def while_waiting(&block)
     self.then do
       Opal::RSpec::AsyncHelpers::ClassMethods.resolve_current_promise
     end
     Opal::RSpec::AsyncHelpers::ClassMethods.get_current_promise_test_instance.run_async &block
   end
-  
+
 end
 
 RSpec.configure do |config|
   config.include ReactTestHelpers
-  config.before(:each) do 
+  config.before(:each) do
     `current_state = {}`
   end
 end
